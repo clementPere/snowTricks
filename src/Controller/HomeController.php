c1b3bb6e32da31db3f9766dee010c4518ba41b20
clementPere <clement.pere.dev@gmail.com>
2022-07-22 14:05:24 +0200

    display all tricks in homePage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Trick;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 
 class HomeController extends AbstractController
 {
+
+    private ManagerRegistry $doctrine;
+    public function __construct(ManagerRegistry $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'tricks' => $this->getTricks(),
         ]);
+    }
+
+    private function getTricks()
+    {
+        $em = $this->doctrine->getManager();
+        return $em->getRepository(Trick::class)->findAll();
     }
 }
