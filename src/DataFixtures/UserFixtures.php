d9a3eea5ca2eb9d45649291a0a9856beb42e755c
clementPere <clement.pere.dev@gmail.com>
2022-09-15 10:12:47 +0200

    add new login page style

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -29,7 +29,7 @@ class UserFixtures extends Fixture
         $user = new User();
         $user->setUsername('admin')
             ->setEmail("admin@test.com")
-            ->setUrlImage("https://picsum.photos/1920/1080?random=132")
+            ->setUrlImage("https://images.pexels.com/photos/848609/pexels-photo-848609.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1")
             ->setRoles(["ROLE_ADMIN"]);
         $password = $this->hasher->hashPassword($user, 'test');
         $user->setPassword($password);
@@ -38,6 +38,18 @@ class UserFixtures extends Fixture
 
     private function addUser(ObjectManager $manager): void
     {
+        $arrayImageUrl = [
+            "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1",
+            "https://images.pexels.com/photos/532220/pexels-photo-532220.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1",
+            "https://images.pexels.com/photos/712513/pexels-photo-712513.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1",
+            "https://images.pexels.com/photos/447329/pexels-photo-447329.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1",
+            "https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1",
+            "https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1",
+            "https://images.pexels.com/photos/834863/pexels-photo-834863.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1",
+            "https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1",
+            "https://images.pexels.com/photos/372042/pexels-photo-372042.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1",
+            "https://images.pexels.com/photos/1181519/pexels-photo-1181519.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080&dpr=1"
+        ];
         $faker = Faker\Factory::create('fr_FR');
         for ($i = 0; $i < 10; $i++) {
             $user = new User();
@@ -45,7 +57,7 @@ class UserFixtures extends Fixture
             $lastname = $faker->lastName;
             $user->setUsername($firstname . ' ' . $lastname)
                 ->setEmail($firstname . '.' . $lastname . '@' . $faker->freeEmailDomain)
-                ->setUrlImage("https://picsum.photos/1920/1080?random=" . $i);
+                ->setUrlImage($arrayImageUrl[$i]);
             $password = $this->hasher->hashPassword($user, $firstname . '.' . $lastname . '1234');
             $user->setPassword($password);
             $manager->persist($user);
